6ac9d9f54c00ba437db18e281d24c264b0d19356
Andndre <agungandre687@gmail.com>
2026-03-31 01:33:13 +0800

    feat: Add pagination configuration file with default views

---
+++ config/pagination.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'default' => 'tailwind',
+
+    'views' => [
+        'tailwind' => 'vendor.pagination.tailwind',
+        'simple-tailwind' => 'vendor.pagination.simple-tailwind',
+        'bootstrap-5' => 'vendor.pagination.bootstrap-5',
+        'simple-bootstrap-5' => 'vendor.pagination.simple-bootstrap-5',
+        'bootstrap-4' => 'vendor.pagination.bootstrap-4',
+        'simple-bootstrap-4' => 'vendor.pagination.simple-bootstrap-4',
+        'default' => 'vendor.pagination.default',
+        'simple-default' => 'vendor.pagination.simple-default',
+    ],
+];
