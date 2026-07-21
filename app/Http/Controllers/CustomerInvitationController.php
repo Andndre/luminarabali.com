a835053de623323573b32c8258fbabb42b0ea49d
Andndre <agungandre687@gmail.com>
2026-07-21 09:09:14 +0800

    feat(7g): halaman daftar tamu + moderasi ucapan buat customer

--- app/Http/Controllers/CustomerInvitationController.php
+++ app/Http/Controllers/CustomerInvitationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\InvitationPage;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class CustomerInvitationController extends Controller
 {
@@ -15,5 +16,26 @@ class CustomerInvitationController extends Controller
             ->get();
 
         return view('invitations-customer.index', compact('invitations'));
+    }
+
+    public function guests($id)
+    {
+        $page = InvitationPage::findOrFail($id);
+        Gate::authorize('update', $page);
+
+        $responses = $page->rsvpResponses()->latest('submitted_at')->get();
+
+        return view('invitations-customer.guests', compact('page', 'responses'));
+    }
+
+    public function toggleRsvpHidden($id, $rsvpId)
+    {
+        $page = InvitationPage::findOrFail($id);
+        Gate::authorize('update', $page);
+
+        $response = $page->rsvpResponses()->findOrFail($rsvpId);
+        $response->update(['is_hidden' => ! $response->is_hidden]);
+
+        return response()->json(['success' => true, 'is_hidden' => $response->is_hidden]);
     }
 }
